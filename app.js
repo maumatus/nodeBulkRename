18098b558aeb1ed56dbb8c328d034dcccd2d6f91
maumatus <86168992+maumatus@users.noreply.github.com>
2021-11-29 10:39:43 -0300

    commit v01-final

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+/*Bulk renamer V01. Documentación al final abajo*/
+
 let fs = require('fs');
 let path = require('path');
 let express = require('express');
 let app = express();
+//let script = require('./public/js/scripts.js')
+
+
+var i = 0;//Esta variable guarda valor de cada iteración de loop "For Each".
+let c = 0;//Esta guarda el valor leído del directorio
+
 
 const port = process.env.PORT || 3000
 
@@ -10,31 +18,35 @@ app.listen(port,()=>console.log(`Ejecutando servidor en http://localhost:${port}
 `press Ctrl-C para terminarlo. `));
 
 //Publicamos carpeta "Public" completa para disponibilidad
-//app.use(express.static('/public/'));
+
 
 app.use(express.urlencoded({
-
     extended: true
-    
 }));
 
 app.use(express.static(__dirname + '/public'));
+app.use("/public/css/", express.static(__dirname + '/public/css/'));
+app.use('/public/img/', express.static(__dirname + '/public/img/'));
+app.use('/public/js/', express.static(__dirname + '/public/js/'));
 //Con método GET user obtiene index.html
 app.get("/",(req,res) => {
-
     res.sendFile(__dirname + '/public/index.html')
-
 });
 
-//Agregaremos un ícono-botón de carpeta que tomara el path para no escribir. Simplifica acceso a data.
-app.post('/carpeta_carga',(req,res)=>{
 
-    let dir_entrada = req.path.dirname('')
-    console.log(dir_entrada)
-
+//***EXPERIMENTAL - Probamos si con AXIOS script.js de FRONT toma el valor de variables con petición GET desde el BACK***//
+app.get("/",(req, res, data) => {
+    DoStuf.DoStuffFunction(req.body.UserID, function(label) {
+        data = i;
+        res.send({message: data});
+        })   
+    //var i = 0;//Esta variable guarda valor de cada iteración de loop "For Each".
+    //let c = 0;//Esta guarda el valor leído del directorio
 });
+//***EXPERIMENTAL***/
 
-/*
+
+
 //Metodo POST. Envía directorio entrada y salida
 app.post("/",(req,res)=>{
 
@@ -42,11 +54,20 @@ app.post("/",(req,res)=>{
     let salida = req.body.salida;
 
     //Probamos que nos reconoce
-    console.log(entrada)
-    console.log(salida)
-//método Node.JS core que lee archivos desde directorio
+    //console.log(entrada)
+    //console.log(salida)
+
+//método Node.JS core que lee todos los archivos ordenados numericamente desde directorio.
     fs.readdir(`${entrada}`,(err, files) => {
-        console.log(files);
+        //Este regex obvia lectura de archivos ocultos extensión .DS_Store
+        files = files.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
+
+//-----------------------------------------------------------------------------
+        //Barra Progreso
+        //Necesitamos contar la cantidad de archivos de directorio. Eso es universo 100%. 
+        c = files.length;//Esta variable guarda el valor de numero de archivos del directorio, descontando archivos ocultos.
+        
+//------------------------------------------------------------------------------
         
         if (!err) {
 
@@ -62,29 +83,43 @@ app.post("/",(req,res)=>{
             ///Users/mauricio/Desktop/salida/nuevo -> Agregar campo para agregar nombre y cargar carpeta con ícono mejor.
                 fs.copyFile(entrada + "/" + file, salida + index + "." + extension,(err)=>{
                     if(err)throw err;
-                    console.log(`archivo ${index} copiado con éxito`)
+                    console.log(`archivo ${index} copiado con éxito`);
+
+                     //Prueba
+                    i = i + 1;
+                    
+                    console.log(i); //Vemos si suma 1 cada vuelta
+                    
+                    //Prueba
+ 
                 });
 
-
-            })//Cierra ForEach
+            });//Cierra ForEach
 
         } else {
             console.log(err)
         }
 
+        console.log(c);
+        console.log(i);
+
     });//Cierra readdir
 
+    res.redirect('http://localhost:3000');
+    
     res.end();
 
-});//Cierre método POST 
+});//Cierre servidor virtual con método POST 
 
-*/
+/*****--Este prototipo aún no en estapa producción, faltan pruebas de carga, pruebas unitarias, etc. 
+funciona bajo NodeJS y Express en el Backend, para Frontend CSS y HTML Markup (Parte del PEVN stack)
 
-//Yo tenía resuelto el como leer campos ingreso desde DOM, revisar e incluir. * OK
-//Falta diseñar interfaz-OK
-//Agregar a un desarrollo con GIT y GITHUB.
-//Pruebas unitarias posteriores
-//Script JS puede validar ingreso
-//Agregar barra de avance
-//Definir entrega producción
+Para usar esta versión de renombrador por lote de imagenes hay que ingresar datos por una "FORM" en la Interfaz principal.
+Se escribe el directorio contenedor de imagenes a copiar y un directorio de salida con el nuevo nombre archivo. 
+La aplicación automaticamente agrega un numero secuencial de acuerdo a orden de carpeta de origen. 
+Estas secuencias yo las uso en Workflow en animación/motion graphics y resuelve el problema de errores en render de nombre o nombres muy largos en secuencias.
+Es Versión 0.1. Posteriormente se planifica agregar barra de avance por streaming de NodeJS y Drop Directory/arrastrar y soltar--******/
+
+
+
 
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,3 @@
-//Aquí ejecutaremos funcionalidades con JS Vanilla ESNext
-/*
-document.getElementById('dir-inicio').onclick = function() {
-    alert("button was clicked");
-    alert('dir-inicio').getAttribute;
- };
-*/
-//alert("Script JS cargó exitosamente");
+//Pendiente.
+
+
